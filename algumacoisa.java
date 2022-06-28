6dcd1a4142365557c842e3d556ec76121d15cb4c
Oquitavio <ocshsdt@gmail.com>
2022-06-28 17:43:56 -0300

    Updade 2 algumacoisa.java
    Segundo update em algumacoisa.java.

--- algumacoisa.java
+++ algumacoisa.java
@@ -6,7 +6,8 @@ public class algumacoisa {
     public static void main (String[] args) {
 
         System.out.println(1+1);
-        System.out.println(2+2);
+        System.out.println(3+3);
+        System.out.println("Matematica");
 
     }
 }
